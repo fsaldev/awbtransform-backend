6dccfca5a46b12c7b17e15126e3547f467f754e6
abdulrehman <abdulrehmanmirza135@gmail.com>
2021-03-30 09:12:57 -0700

    server

--- main2.py
+++ main2.py
@@ -13,16 +13,16 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 app.config['MONGODB_SETTINGS'] = {
+   # 'db': 'awbtransport',
+   #'host': 'mongodb+srv://test:test1234@test.iocw1.mongodb.net/awbTransport1',
     'db': 'awbtransport',
-    'host': 'mongodb+srv://test:test1234@test.iocw1.mongodb.net/awbTransport1',
-    # 'db': 'awbtransport',
-    # 'host': 'localhost',
+     'host': 'localhost',
     'port': 27017
 }
 db = MongoEngine()
 db.init_app(app)
-PDFKIT_CONFIGURATION  = pdfkit.configuration(wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
-# PDFKIT_CONFIGURATION = pdfkit.configuration(wkhtmltopdf="/home/awbtransport/wkhtml-install/usr/local/bin/wkhtmltopdf")
+#PDFKIT_CONFIGURATION  = pdfkit.configuration(wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
+PDFKIT_CONFIGURATION = pdfkit.configuration(wkhtmltopdf="/home/awbtransport/wkhtml-install/usr/local/bin/wkhtmltopdf")
 
 ######################################Start Models########################################################
 
